6e36594bcb3c07f2eb5174ced4536c5a9462dfd7
Lydia <lydiakristinee@outlook.com>
2023-07-24 10:59:15 +0930

    Created command folder levels. Create command file addxp.js. Addxp.js contains slash command to manually add xp to a user.

---
+++ commands/levels/addxp.js
@@ -0,0 +1,28 @@
+const { SlashCommandBuilder } = require('@discord.js/builders');
+const connection = require ('../../database');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('addxp')
+        .setDescription('Add XP to a user.')
+        .addIntegerOption(option =>
+            option.setName('amount')
+                .setDescription('The amount of XP to add.')
+                .setRequired(true))
+        .addUserOption(option =>
+            option.setName('user')
+                .setDescription('The user to add XP to.')
+                .setRequired(true)),
+    async execute(interaction) {
+        const amount = interaction.options.getInteger('amount');
+        const user = interaction.options.getUser('user');
+        connection.execute(
+            'INSERT INTO user (id, xp) VALUES (?, ?) ON DUPLICATE KEY UPDATE    xp = xp + ?',
+            [user, amount, amount],
+            (err, result) => {
+                if (err) throw err;
+                interaction.reply('${amount}XP added to ${user}!')
+            }
+        );
+    },
+};
