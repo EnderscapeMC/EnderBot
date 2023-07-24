f6168c4bd0c13b459248258a84453101f39fe842
Lydia <lydiakristinee@outlook.com>
2023-07-24 13:24:58 +0930

    Changed console log message.

--- commands/levels/addxp.js
+++ commands/levels/addxp.js
@@ -17,7 +17,7 @@ module.exports = {
         const amount = interaction.options.getInteger('amount');
         const user = interaction.options.getUser('user');
         const member = interaction.member;
-        console.log(`${member.displayName} added ${amount} XP to ${user.username}.`);
+        console.log(`${interaction.user.username} added ${amount} XP to ${user.username}.`);
         connection.query(
             'INSERT INTO users (id, xp, username, discriminator, avatar) VALUES (?, ?, ?, ?, ?) ON DUPLICATE KEY UPDATE xp = xp + ?',
             [user.id, amount, user.username, user.discriminator, user.avatar, amount],
