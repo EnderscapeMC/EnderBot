d128bc87a57d5e3c9f135dc253fdd262bbf6fa7d
Lydia <lydiakristinee@outlook.com>
2023-07-25 21:04:31 +0930

    Testing commiting.

--- commands/utility/changelog.js
+++ commands/utility/changelog.js
@@ -17,7 +17,7 @@ module.exports = {
                 .addFields(
                     { name: '\u200B', value: '➤ **Miscellaneous:**\n• Nations levels added to `/levelperks` - can now also use `/levels` to open this menu. Nations items do not currently update.\n• `/rankperks` menu items now change depending on player rank.', inline: false },
                     { name: '\u200B', value: '➤ **Bug Fixes:**\n• Fixed issue whereby players had insufficient permissions to upgrade Upgradeable Hoppers.\n• Fixed butcher drops for Blaze spawners, as reported by <@529311057437655050>.', inline: false },
-                );
+                ); 
             
             interaction.channel.send({ embeds: [changelogEmbed] });
         }
